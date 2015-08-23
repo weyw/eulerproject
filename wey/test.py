c72b6a6f3faa7faf5d2524425c5b53130b0466ed
Wey Wang <wey@weywang.com>
2015-08-23 16:39:45 -0700

    w: p45

--- wey/test.py
+++ wey/test.py
@@ -3,7 +3,6 @@
 
 from euler import *
 
-pandigital = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
-p = 2143
-print len(set(str(p))), len(str(p)), set('123456789') >= set(str(p)), set(str(p))
+n = 3976
 
+print n * (3 * n - 1) / 2, n * (2 * n - 1)
